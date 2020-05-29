8d4e62c5dbd959436197900c134c135ced4ab42c
emmaCIT <nneoma.ogbene@mycit.ie>
2020-05-29 12:52:59 +0100

    adding message

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const mongoose= require('mongoose');
 const cors =require("cors");
+const path = require ('path');
 require('dotenv').config();
 const app= express();
 
@@ -10,7 +11,7 @@ const patientRouter= require("./routes/patient");
 const doctorRouter = require("./routes/doctor");
 const db_url=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-r2chw.mongodb.net/diabates?retryWrites=true&w=majority`;
 
-mongoose.connect(db_url,{useNewUrlParser: true,useUnifiedTopology: true } )
+mongoose.connect(process.env.MONGODB_URI||db_url,{useNewUrlParser: true,useUnifiedTopology: true } )
     .then(() =>{
          console.log("db connected");
     }).catch(e=>{
@@ -25,4 +26,12 @@ app.use(express.json())
 app.use("/api/user",authRouter);
 app.use("/api/patient",patientRouter);
 app.use("/api/doctor",doctorRouter)
-app.listen(3003,()=>{ console.log("app runing on Port 3002");});
+app.listen(process.env.port||3003,()=>{ console.log("app runing on Port 3002");});
+
+if(process.env.NODE_ENV=== 'production'){
+    app.use(express.static('diabates_management/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname,'diabates_management', 'build','index.html'));
+    });
+
+}
